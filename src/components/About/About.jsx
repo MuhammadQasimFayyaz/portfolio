40240d72d22ae1e50403fb1ec2353bd63a8a095e
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-08-25 14:06:13 +0500

    about section added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import './About.css'
+import design from '../../assets/design.svg'
+
+function About() {
+  return (
+    <div className='about'>
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={design} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src="" alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>I am an experienced Frontend Developer with over a decade of professional expertise in the field. Throughout my career, I have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.</p>
+            <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill"><p>HTML & CSS</p><hr style={{width: '80%'}}/></div>
+            <div className="about-skill"><p>JavaScript</p><hr style={{width: '70%'}}/></div>
+            <div className="about-skill"><p>ReactJS</p><hr style={{width: '60%'}}/></div>
+            <div className="about-skill"><p>WordPress</p><hr style={{width: '70%'}}/></div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>Years of experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>Projects completed</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>happy clients</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About

0991db28ba59a73b6ec3dc6e88536640567b03a7
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-08-27 12:47:40 +0500

    send email functionality added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,30 @@ import phone from '../../assets/phone.svg'
 import location from '../../assets/location.svg'
 
 function Contact() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "871a738d-73da-4297-82d5-1b61d58e17c3");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
+  };
+
+
   return (
     <div id='contact' className='contact'>
       <div className="contact-title">
@@ -30,7 +54,7 @@ function Contact() {
             </div>
           </div>
         </div>
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
           <label htmlFor="name">your name</label>
           <input type="text" name="name" id="name" placeholder='Enter your name' />
           <label htmlFor="email">your email</label>

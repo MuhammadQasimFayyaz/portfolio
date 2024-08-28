3b816dfaeaefc29b04bd7348f51bcf63a147218a
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-08-28 13:03:39 +0500

    make 2 sections responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,61 @@
-import { useState } from 'react'
-// import './Navbar.css'
+import { useRef, useState } from 'react'
+import './Navbar.css'
 import underline from '../../assets/underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_bar from '../../assets/menu.svg'
+import close from '../../assets/close.svg'
 
 function Navbar() {
 
   const [menu, setMenu] = useState("home")
+  const menuRef = useRef()
+
+  const openMenu = () => {
+    menuRef.current.style.right = '0'
+  }
+  const closeMenu = () => {
+    menuRef.current.style.right = '-350px'
+  }
 
   return (
-    <div className='flex items-center justify-between mx-[170px] my-[20px]'>
+    <div className='navbar flex items-center justify-between mx-[170px] my-[20px]'>
       <a href="/" className="text-white text-[20px] no-underline">Muhammad Qasim Fayyaz</a>
-      <ul className='flex items-center list-none gap-[60px] text-[20px]'>
+      <img src={menu_bar} alt="menu bar" className='nav-mob-open' onClick={openMenu} />
+      <ul ref={menuRef} className='nav-menu flex items-center list-none gap-[60px] text-[20px]'>
+        <img className='nav-mob-close' src={close} alt="" onClick={closeMenu} />
         <li className='flex flex-col gap-[5px] cursor-pointer'>
-          <AnchorLink className='no-underline text-white' href='#home'>
+          <AnchorLink className='anchor-link no-underline text-white' href='#home'>
             <p onClick={() => setMenu("home")}>Home</p>
           </AnchorLink>
           {menu === "home" ? <img src={underline} alt="" className="flex mx-auto" /> : <></>}
         </li>
         <li className='flex flex-col gap-[5px] cursor-pointer'>
-          <AnchorLink className='no-underline text-white' offset={50} href='#about'>
+          <AnchorLink className='anchor-link no-underline text-white' offset={50} href='#about'>
             <p onClick={() => setMenu("about")}>About me</p>
           </AnchorLink>
           {menu === "about" ? <img src={underline} alt="" className="flex mx-auto" /> : <></>}
         </li>
         <li className='flex flex-col gap-[5px] cursor-pointer'>
-          <AnchorLink className='no-underline text-white' offset={50} href='#services'>
+          <AnchorLink className='anchor-link no-underline text-white' offset={50} href='#services'>
             <p onClick={() => setMenu("services")}>Services</p>
           </AnchorLink>
           {menu === "services" ? <img src={underline} alt="" className="flex mx-auto" /> : <></>}
         </li>
         <li className='flex flex-col gap-[5px] cursor-pointer'>
-          <AnchorLink className='no-underline text-white' offset={50} href='#work'>
+          <AnchorLink className='anchor-link no-underline text-white' offset={50} href='#work'>
             <p onClick={() => setMenu("work")}>Portfolio</p>
           </AnchorLink>
           {menu === "work" ? <img src={underline} alt="" className="flex mx-auto" /> : <></>}
         </li>
         <li className='flex flex-col gap-[5px] cursor-pointer'>
-          <AnchorLink className='no-underline text-white' offset={50} href='#contact'>
+          <AnchorLink className='anchor-link no-underline text-white' offset={50} href='#contact'>
             <p onClick={() => setMenu("contact")}>Contact</p>
           </AnchorLink>
           {menu === "contact" ? <img src={underline} alt="" className="flex mx-auto" /> : <></>}
         </li>
       </ul>
-      <AnchorLink className='no-underline text-white' offset={50} href='#contact'>
-        <div className="py-[20px] px-[40px] rounded-full bg-gradient-to-r from-[#da7c25] to-[#b923e1] text-[22px] cursor-pointer transition-transform duration-500 hover:scale-105">
+      <AnchorLink className='anchor-link no-underline text-white' offset={50} href='#contact'>
+        <div className="nav-connect py-[20px] px-[40px] rounded-full bg-gradient-to-r from-[#da7c25] to-[#b923e1] text-[22px] cursor-pointer transition-transform duration-500 hover:scale-105">
           Connect with me
         </div>
       </AnchorLink>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,28 +5,28 @@ import right_arrow from '../../assets/right_arrow.svg'
 
 function Services() {
   return (
-    <div id='services' className="flex flex-col items-center justify-center gap-[80px] mx-[170px] my-[80px]">
-      <div className="relative">
-        <h1 className="relative px-[30px] text-[80px] font-semibold">My Services</h1>
-        <img src={design} alt="" className="absolute bottom-0 right-0 z-[-1]" />
+    <div id='services' className="services flex flex-col items-center justify-center gap-[80px] mx-[170px] my-[80px] md:items-start md:gap-[70px] md:mx-[70px] md:my-[20px]">
+      <div className="services-title relative">
+        <h1 className="relative px-[30px] text-[80px] font-semibold md:text-[50px] md:p-0">My Services</h1>
+        <img src={design} alt="" className="absolute bottom-0 right-0 z-[-1] md:w-[130px] md:mr-[-20px]" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-[40px] mb-[80px]">
+      <div className="services-container grid grid-cols-1 md:grid-cols-2 gap-[40px] mb-[80px] md:gap-[30px]">
         {services_data.map((service, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col justify-center gap-[20px] px-[30px] py-[60px] rounded-[10px] border-2 border-white transition-all duration-400 cursor-pointer hover:border-[#ff00ff] hover:bg-gradient-to-br from-[#3f0028] to-[#582300] transform hover:scale-[1.05]"
+              className="services-format flex flex-col justify-center gap-[20px] px-[30px] py-[60px] rounded-[10px] border-2 border-white transition-all duration-400 cursor-pointer hover:border-[#ff00ff] hover:bg-gradient-to-br from-[#3f0028] to-[#582300] transform hover:scale-[1.05] md:gap-[10px] md:p-[20px]"
             >
               <h3 className="text-[28px] font-semibold">{service.s_no}</h3>
-              <h2 className="text-[38px] font-extrabold bg-gradient-to-r from-[#df8908] to-[#b415ff] bg-clip-text text-transparent">
+              <h2 className="text-[38px] font-extrabold bg-gradient-to-r from-[#df8908] to-[#b415ff] bg-clip-text text-transparent md:text-[20px]">
                 {service.s_name}
               </h2>
-              <p className="text-[#d4d4d4] text-[24px] leading-[40px] max-w-[300px]">
+              <p className="text-[#d4d4d4] text-[24px] leading-[40px] max-w-[300px] md:text-[16px] md:leading-[26px]">
                 {service.s_desc}
               </p>
-              <div className="flex gap-[20px] items-center mt-[20px]">
-                <p>Read More</p>
-                <img src={right_arrow} alt="" />
+              <div className="services-readmore flex gap-[20px] items-center mt-[20px]">
+                <p className='md:text-[16px]'>Read More</p>
+                <img src={right_arrow} alt="" className="md:w-[20px]" />
               </div>
             </div>
           )

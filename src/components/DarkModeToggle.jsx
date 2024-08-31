b5ff676771773e60a07ff2d6f833dc16e1c59f40
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-08-31 12:53:14 +0500

    fixes some bugs

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -26,9 +26,9 @@ function DarkModeToggle() {
       className="fixed right-0 top-[50%] translate-y-[-50%] p-2 rounded-l-full border bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200"
     >
       {darkMode ? (
-        <FaSun className="text-yellow-500 h-6 w-6" />
-      ) : (
         <FaMoon className="text-gray-500 h-6 w-6" />
+      ) : (
+        <FaSun className="text-yellow-500 h-6 w-6" />
       )}
     </button>
   );
